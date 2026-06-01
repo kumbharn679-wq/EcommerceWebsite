8c6b27660020d6b3cf38ac41e65c869f107daefa
Nikita Kumbhar <kumbharn679@gmail.com>
2026-06-01 11:57:20 +0530

    Update fetchQuantityFromCartLS.js

--- fetchQuantityFromCartLS.js
+++ fetchQuantityFromCartLS.js
@@ -2,7 +2,7 @@ import { getCartProductFormLS } from "./getCartProducts";
 
 export const fetchQuantityFromCartLS = (id ,price) => {
    let cartProducts = getCartProductFormLS();
-    let existingProduct = cartProducts.find((curProd) => curProd.id === id); // this line is used to find the product with the specified id in the cartProducts array, which represents the products currently in the cart. The find method is used to search for a product object in the array that has a matching id, and if such an object is found, it is stored in the existingProd variable. This allows us to retrieve the quantity and price information for that specific product from the cart products stored in local storage.
+    let existingProduct = cartProducts.find((curProd) => curProd.id === id);
      let quantity = 1;
      if(existingProduct){
         quantity = existingProduct.quantity;
