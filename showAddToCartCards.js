045428a4818ee3367cd60788b4b3015f2127708a
Nikita Kumbhar <kumbharn679@gmail.com>
2026-06-01 12:01:06 +0530

    Update showAddToCartCards.js

--- showAddToCartCards.js
+++ showAddToCartCards.js
@@ -10,7 +10,7 @@ import { updateCartValues } from "./updateCartValue";
 let cartProducts = getCartProductFormLS();
 
 let filterProducts = products.filter((curProd) => {
-     return cartProducts.some((curElem) => curElem.id === curProd.id); // this is  used to filter the products array based on the products that are currently in the cart. The filter method is used to iterate through each product in the products array, and for each product, the some method is used to check if there is a matching product in the cartProducts array (which represents the products currently in the cart) based on their id. If a match is found, the product is included in the filterProducts array, which will contain only the products that are currently in the cart. This allows us to easily display or manipulate the products that are relevant to the user's cart.
+     return cartProducts.some((curElem) => curElem.id === curProd.id); 
     //console.log(curProd.name); 
 });
 
@@ -20,15 +20,15 @@ const cartElement = document.querySelector("#productCartContainer");
 const templateContainer = document.querySelector("#productCartTemplate");
 
  const showCartProduct = () => { 
-    filterProducts.forEach((curProd) => { // this line is used to iterate through each product in the filterProducts array, which contains the products that are currently in the cart. For each product, the code inside the forEach loop will be executed, allowing us to create and display a product card for each item in the cart on the webpage.
+    filterProducts.forEach((curProd) => { 
         const {  category, id, image, name, stock, price} = curProd;
-       let productClone = document.importNode(templateContainer.content, true);// content is used to access the content of the template element and true is used for deep cloning which means it will clone all the child elements of the template. This allows us to create a new product card for each product in the products array by cloning the template and populating it with the product data. 
-    let lsActualData  = fetchQuantityFromCartLS(id, price); // this line is used to fetch the quantity and price data for the current product from the cart products stored in local storage. The fetchQuantityFromCartLS function takes the product id and price as arguments and retrieves the corresponding quantity and price information for that product from the cart products array in local storage. This allows us to display the correct quantity and price for each product in the cart when we create the product card using the template.
+       let productClone = document.importNode(templateContainer.content, true);
+    let lsActualData  = fetchQuantityFromCartLS(id, price); 
      
        productClone.querySelector("#cardValue").setAttribute("id", `card${id}`);
 
     productClone.querySelector(".category").textContent = category;
-    productClone.querySelector(".productName").textContent = name; // this line is used to set the text content of the element with the class name "productName" to the value of the variable "name" which is obtained from the current product object in the products array. This allows us to display the name of each product on the webpage.
+    productClone.querySelector(".productName").textContent = name;
     productClone.querySelector(".productImage").src = image;
  
    
@@ -36,7 +36,7 @@ const templateContainer = document.querySelector("#productCartTemplate");
    productClone.querySelector(".productPrice").textContent = lsActualData.price;
     
    productClone.querySelector(".remove-to-cart-button").addEventListener("click", () => 
-        removeProdFromCart(id)); // this line is used to add a click event listener to the element with the class name "remove-to-cart-button" within the cloned product card. When the button is clicked, it will call the removeProdFromCart function and pass the product
+        removeProdFromCart(id)); 
 
       
 
@@ -45,7 +45,7 @@ const templateContainer = document.querySelector("#productCartTemplate");
  productClone.querySelector(".stockElement").addEventListener("click",(event) => {
    incrementDecrement(event,stock,id,price);
  });
-   cartElement.appendChild(productClone);// this line is used to append the cloned product card to the product container element in the DOM, which allows us to display all the products on the webpage as cards.
+   cartElement.appendChild(productClone);
 
  })
    
