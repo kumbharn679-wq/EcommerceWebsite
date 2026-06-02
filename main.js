f9bbd014cd1758337a8ab5a6e0f37b65d8f81533
Nikita <kumbharn679@gmail.com>
2026-06-02 10:14:52 +0530

    Fix homeProductCards import

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import './style.css';
 //import products from "./api/products.json";
-import { showProductContainer } from './homeProductCards';
+import { showProductContainer } from './homeProductCards.js';
 // console.log(products);
 //call the function to display all the products as a card
 //showProductContainer(products);
