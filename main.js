633997faf62869c17ef56bf30aa95e1a3c478fb5
Nikita <kumbharn679@gmail.com>
2026-06-02 10:00:04 +0530

    debug products

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 import './style.css';
-import products from "./api/products.json";
+//import products from "./api/products.json";
 import { showProductContainer } from './homeProductCards';
 // console.log(products);
 //call the function to display all the products as a card
+//showProductContainer(products);
+import products from "./api/products.json";
+
+console.log("PRODUCTS DATA:", products);
+
 showProductContainer(products);
