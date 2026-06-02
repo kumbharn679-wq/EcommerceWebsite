c4d0c01c24204f65e9558e4a7ee39e4fea7bf8a6
Nikita <kumbharn679@gmail.com>
2026-06-02 08:55:52 +0530

    Resolved conflicts

--- homeQuantityToggle.js
+++ homeQuantityToggle.js
@@ -3,13 +3,7 @@ export const homeQuantityToggle = (event,id,stock) => {
 const currentCardElement = document.querySelector(`#card${id}`);
 // console.log(currentCardElement); 
  const productQuantity = currentCardElement.querySelector(".productQuantity");
-<<<<<<< HEAD
- //console.log(productQuantity); 
- let quantity =parseInt(productQuantity.getAttribute("data-quantity")) || 1;    
-=======
- //console.log(productQuantity);
- let quantity =parseInt(productQuantity.getAttribute("data-quantity")) || 1;   
->>>>>>> 2808469de8c649a25664f26f3e4d48f9b5bc8a50
+let quantity = parseInt(productQuantity.getAttribute("data-quantity")) || 1;
  if(event.target.className === "cartIncrement"){
    if(quantity < stock){
         quantity += 1 ;
@@ -18,11 +12,7 @@ const currentCardElement = document.querySelector(`#card${id}`);
  }
  }
 
-<<<<<<< HEAD
- if((event.target.className === "cartDecrement")){  
-=======
- if((event.target.className === "cartDecrement")){ 
->>>>>>> 2808469de8c649a25664f26f3e4d48f9b5bc8a50
+ if((event.target.className === "cartDecrement")){
     if(quantity > 1){
         quantity -= 1; 
     }
@@ -31,10 +21,6 @@ const currentCardElement = document.querySelector(`#card${id}`);
 
  productQuantity.innerText = quantity; 
  console.log(quantity); 
-<<<<<<< HEAD
- productQuantity.setAttribute('data-quantity',quantity);
-=======
  productQuantity.setAttribute('data-quantity',quantity); 
->>>>>>> 2808469de8c649a25664f26f3e4d48f9b5bc8a50
 return quantity ; 
 }
